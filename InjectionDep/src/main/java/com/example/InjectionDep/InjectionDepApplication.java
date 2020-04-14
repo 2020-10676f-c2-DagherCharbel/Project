92510f63b7e9550e4ef7538b3b3f3c9a9a35a336
charbel dagher <charbel.dagher@isae.edu.lb>
2020-04-14 16:35:10 +0300

    Add files via upload

---
+++ InjectionDep/src/main/java/com/example/InjectionDep/InjectionDepApplication.java
@@ -0,0 +1,13 @@
+package com.example.InjectionDep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectionDepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjectionDepApplication.class, args);
+	}
+
+}
